c369265c196845cf2cba83896aa34bc24b04aab0
Pratyush-A <pratyushacharya34@gmail.com>
2025-10-30 01:03:20 +0530

    Deployement Fix

--- src/components/Today task.tsx
+++ src/components/Today task.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const TodayTask = () => {
   const [activeTab, setActiveTab] = useState("All");
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { NavLink } from "react-router-dom";
 import SidebarLogo from "/assets/sidebar.png";
 import { Plus, CircleHelp, LayoutGrid, Briefcase, ListChecks, Menu, X } from "lucide-react";
